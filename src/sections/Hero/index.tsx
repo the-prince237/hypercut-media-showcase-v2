a2086316415ef118d2245bc8c9ab5db4f24344b0
= <=>
2024-02-21 23:53:48 +0100

    basis

--- src/sections/Hero/index.tsx
+++ src/sections/Hero/index.tsx
@@ -1,8 +1,12 @@
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <Box className='absolute w-screen h-screen max-w-screen max-h-screen px-[196px] flex flex-col justify-center border-solid border-white border-2'>
+      <Typography className='text-[250px]'>PRENEZ LE CONTRÔLE</Typography>
+      <Typography className='text-white font-alfa-slab text-[80px]'>Sur Les Réseaux</Typography>
+    </Box>
   )
 }
 
